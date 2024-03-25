a587353efb35aa63d37a8f07e5d815e39f2c0824
Viktoria <viktoruytarhova@gmail.com>
2024-03-25 23:13:11 +0200

    3

--- Program.cs
+++ Program.cs
@@ -25,16 +25,63 @@
 // 8 => 2, 4, 6, 8
 
 
-System.Console.Write("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+// System.Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
 
-while (i <= N)
-{
-    if (i % 2 == 0)
-    {
-        System.Console.Write(i + " ");
-    }
-    i ++;
+// while (i <= N)
+// {
+//     if (i % 2 == 0)
+//     {
+//         System.Console.Write(i + " ");
+//     }
+//     i ++;
 
-}
+// }
+
+
+// второй способ
+
+//  for (int i = 2; i <= N; i +=2)
+//  {
+//     System.Console.Write(i + " ");
+//  }
+
+
+// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+
+// Примеры
+// a = 256 => 26
+// a = 891 => 81\
+
+// using System.Runtime.InteropServices;
+
+// System.Console.Write("Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// // ------100-----999--->
+
+// if(number > 99 && number < 1000)
+// {
+//    int leftDigit = number / 100;
+//    int rigtDigit = number % 10;
+//    int newNumber = leftDigit * 10 + rigtDigit;
+//    System.Console.Write(newNumber); 
+// }
+// else
+// {
+//     System.Console.Write("Вы ввели не трехзначное число!");
+// }
+
+
+
+// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
